9a12c7b239cc1d72f693d0e9fe2cf2253c71375c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-24 22:36:12 +0200

    Fix `WP_TESTS_TAG`

--- 7.0/install.sh
+++ 7.0/install.sh
@@ -24,6 +24,8 @@ DB_USER=root
 DB_PASS=''
 DB_HOST=mysql
 
+set -ex
+
 download() {
     if [ `which curl` ]; then
         curl -s "$1" > "$2";
@@ -32,23 +34,23 @@ download() {
     fi
 }
 
-if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
-  WP_TESTS_TAG="tags/$WP_VERSION"
-elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-  WP_TESTS_TAG="trunk"
-else
-  # http serves a single offer, whereas https serves multiple. we only want one
-  download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
-  grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
-  LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-  if [[ -z "$LATEST_VERSION" ]]; then
-    echo "Latest WordPress version could not be found"
-    exit 1
+prepare() {
+  if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
+    WP_TESTS_TAG="tags/$WP_VERSION"
+  elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+    WP_TESTS_TAG="trunk"
+  else
+    # http serves a single offer, whereas https serves multiple. we only want one
+    download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+    grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
+    LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
+    if [[ -z "$LATEST_VERSION" ]]; then
+      echo "Latest WordPress version could not be found"
+      exit 1
+    fi
+    WP_TESTS_TAG="tags/$LATEST_VERSION"
   fi
-  WP_TESTS_TAG="tags/$LATEST_VERSION"
-fi
-
-set -ex
+}
 
 install_wp() {
   if [ -d $WP_CORE_DIR_VERSION ]; then
@@ -107,6 +109,7 @@ for ver in "${versions_to_install[@]}"; do
 	WP_TESTS_DIR_VERSION=/tmp/wordpress-tests-lib-$ver
 	WP_CORE_DIR_VERSION=/tmp/wordpress-$ver/
 
+  prepare
 	install_wp
 	install_test_suite
 done
