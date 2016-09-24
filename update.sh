103c202bac4be27690c95a3a848554f2f50e8787
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-09-24 22:35:42 +0200

    Fix permissions

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 cat versions.sh <(cat install-tmpl.sh) > install.sh
 
+chmod +x install.sh
+chmod +x docker-entrypoint.sh
+
 for d in */; do
   echo "Updating $d with new scripts"
   cp install.sh $d
